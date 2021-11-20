d585a56a8b8ce786ed09f0c67ff6c0050c15fdb4
hp3icc <49044505+hp3icc@users.noreply.github.com>
2021-11-19 22:34:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ wget https://github.com/hp3icc/D-APRS/raw/main/user_settings.txt
 sudo cat > /bin/menu-daprs <<- "EOF"
 #!/bin/bash
 while : ; do
-choix=$(whiptail --title "TE1ws-Rev12a Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 16 65 5 \
+choix=$(whiptail --title "D-APRS KF7EEL / Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 16 65 5 \
 1 " Editar igate" \
 2 " Iniciar Igate " \
 3 " Detener Igate " \
