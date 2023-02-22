9c061d6fd900eabefc648efd3d2aae16f8582834
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-21 20:27:53 -0500

    Create emq-te1+.sh

---
+++ emq-te1+.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+apt update
+sudo apt-get install git -y
+sudo apt install python3-pip -y
+#
+cd /home/
+sudo cat > /home/requirements.txt <<- "EOF"
+bitstring>=3.1.5
+bitarray>=0.8.1
+Twisted>=16.3.0
+dmr_utils3>=0.1.19
+configparser>=3.0.0
+aprslib>=0.6.42
+pynmea2
+maidenhead
+flask
+folium
+mysql-connector
+resettabletimer>=0.7.0
+setproctitle
+requests
+libscrc
+resettabletimer
+cryptography
+
+EOF
+##
+pip3 install -r requirements.txt
+sudo rm requirements.txt
+cd /opt/
+git clone https://github.com/hp3icc/D-APRS.git
+
+sudo cat > /bin/menu-igate <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "D-APRS KF7EEL / Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 16 65 7 \
+1 " Editar igate" \
+2 " Iniciar Igate " \
+3 " Detener Igate " \
+4 " Dashboard on " \
+5 " Dashboard off " \
+6 " Salir del menu " 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+nano /opt/D-APRS/gps_data.cfg ;;
+2)
+sudo systemctl stop daprs.service && sudo systemctl start daprs.service && sudo systemctl enable daprs.service ;;
+3)
+sudo systemctl stop daprs.service && sudo systemctl disable daprs.service ;;
+4)
+sudo systemctl stop daprs-board.service && systemctl start daprs-board.service && sudo systemctl enable daprs-board.service ;;
+5)
+sudo systemctl stop daprs-board.service && sudo systemctl disable daprs-board.service ;;
+6)
+break;
+esac
+done
+exit 0
+EOF
+#
+sudo cat > /lib/systemd/system/daprs.service <<- "EOF"
+[Unit]
+Description=Data bridge APRS
+After=network-online.target syslog.target
+Wants=network-online.target
+
+[Service]
+StandardOutput=null
+WorkingDirectory=/opt/D-APRS
+RestartSec=3
+ExecStart=/usr/bin/python3 /opt/D-APRS/gps_data.py
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+
+
+EOF
+#
+sudo cat > /lib/systemd/system/daprs-board.service <<- "EOF"
+[Unit]
+Description=Dashboard D-APRS
+After=network-online.target syslog.target
+Wants=network-online.target
+
+[Service]
+StandardOutput=null
+WorkingDirectory=/opt/D-APRS/dashboard
+RestartSec=3
+ExecStart=/usr/bin/python3 /opt/D-APRS/dashboard/dashboard.py -c /opt/D-APRS/gps_data.cfg
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+
+
+EOF
+#
+sudo chmod +777 /opt/D-APRS/user_settings.txt
+sudo chmod +x /opt/D-APRS/dashboard/*.py
+sudo chmod +x /opt/D-APRS/*.py
+sudo chmod +x /bin/menu-igate
+sudo chmod 755 /lib/systemd/system/daprs-board.service
+sudo chmod 755 /lib/systemd/system/daprs.service
+sudo systemctl daemon-reload
+
+
